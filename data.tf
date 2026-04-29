4e00262b8fdf7f3934ea2a81af60188754a71b06
Uriah Raffer <umraffer32@gmail.com>
2026-04-28 21:39:07 -0700

    added data for ubuntu and debian images

---
+++ data.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "debian" {
+  most_recent = true
+  owners      = ["136693071363"]  # Debian's official AWS account
+
+  filter {
+    name   = "name"
+    values = ["debian-${var.debian_version}-amd64-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]  # Canonical's official AWS account
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-*-${var.ubuntu_version}-amd64-server-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
